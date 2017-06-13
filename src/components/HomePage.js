c2c71d47f9fb370858ea07f08c56be07e350a50e
swedendrift <swedendrift@gmail.com>
2017-06-13 05:26:13 -0700

    icons chosen working on react router

---
+++ src/components/HomePage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const HomePage = () => {
+
+  
+
+  return (
+    <div>
+        <div id="logo-name">
+          LÆTHOS
+        </div>
+        <div id="icons">
+          <a href=""><img className="icon" id="innovate" src="./images/innovate.svg" alt="Innovate" /></a>
+          <a href=""><img className="icon" id="create" src="./images/create.svg" alt="Create" /></a>
+          <a href=""><img className="icon" id="operate" src="./images/operate.svg" alt="Operate" /></a>
+        </div>
+    </div>
+  );
+};
+
+export default HomePage;
