e1918977f57f0397495518d9cdf5e1608cb2c5cf
swedendrift <swedendrift@gmail.com>
2017-07-22 23:57:29 -0700

    reviewing and minor changes

--- src/components/CreatePage.js
+++ src/components/CreatePage.js
@@ -3,12 +3,7 @@ import { Link } from 'react-router-dom';
 
 const CreatePage  = () => (
   <div className="create">
-    <div>
-      <Link to="/"><img className="icon logo" id="home" src="./images/Laethos112x191.gif" alt="Laethos Home" /></Link>
-    </div>
-    <div className="App">
-      <h1 className="pageTitle">Create</h1>
-    </div>
+    <Link to="/"><img className="icon" id="logo" src="./images/Laethos112x191.gif" alt="Laethos Home" /></Link>
   </div>
 );
 
